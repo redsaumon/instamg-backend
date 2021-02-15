d3f664364cb4937c2c6e43e643850c3b4bf311d5
redsaumon <ywhong830@gmail.com>
2021-02-15 18:30:23 +0900

    Add: 초기세팅

---
+++ decorator.py
@@ -0,0 +1,36 @@
+import json
+import jwt
+import datetime
+
+from django.http import JsonResponse
+
+from my_settings import SECRET
+from users.models import User
+
+# 로그인 체크
+def login_check(func):
+    def wrapper(self, request, *args, **kwargs):
+        #data       = json.loads(request.body)
+        try:
+            token      = request.headers.get('Authorization')
+            user_token = jwt.decode(token, SECRET, algorithms='HS256')
+            print(request.headers)
+            print(user_token)
+
+            setattr(request, 'user', User.objects.get(id=user_token['id']))
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message':'INVALID_TOKEN'}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({'message':'INVALID_USER'}, status=400)
+        return func(self, request, *args, **kwargs)
+    return wrapper
+
+# KeyError
+def key_error_killer(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            print('keyError decorator입니다.')
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+        return func(self, request, *args, **kwargs)
+    return wrapper
---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'instamg',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+    }
+}
+
+SECRET = '*n9p4!b!l&cmvq_t$e4e8h_+uoqfai%pa2u^u&)!giutkb-r%)'
